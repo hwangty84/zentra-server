ba6f58a0e424c69f326d3f01ad3456a5e6e4c011
Willy Hwang <hwangty84@gmail.com>
2025-07-31 12:02:23 +0900

    초기 커밋

--- crawl-server.js
+++ crawl-server.js
@@ -1,14 +1,14 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: "new", // 최신 버전에서 권장되는 설정
-    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Render 환경에서 필수
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
-  await page.goto("https://example.com");
+  await page.goto('https://example.com');
+  console.log(await page.title());
 
-  // ...
   await browser.close();
 })();
